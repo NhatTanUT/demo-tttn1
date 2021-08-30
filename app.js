6395520d8bc815b90aa039936824a5560bf0ecfc
NhatTanUT <tvnt221@gmail.com>
2021-08-30 17:00:23 +0700

    test reg 8

--- app.js
+++ app.js
@@ -45,6 +45,11 @@ app.use(session({
 // ================ ROUTE ===================
 require('./routes/auth')(app, passport)
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
 
 app.get('/product', async function(req, res) {
     const data = await Product.find({}).populate('previewImage')
