26529336123c84fc0d741a6cb6ecc64a7ee034f5
NhatTanUT <tvnt221@gmail.com>
2021-09-08 18:59:03 +0700

    3

--- app.js
+++ app.js
@@ -31,22 +31,6 @@ const Product = require('./models/product.model')
 const Category = require('./models/category.model')
 const PreviewImage = require('./models/previewImage.model')
 
-
-app.use(function(req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
-    next();
-  });
-
 require('./config/passport')(passport)
 
 // =========== SETUP SESSION ========================
