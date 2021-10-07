75ce616ca71816ed4f406fcf4705d0686c2df568
NhatTanUT <tvnt221@gmail.com>
2021-10-07 10:27:27 +0700

    fix2

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -323,7 +323,7 @@ class UserController {
     </tr>
     <tr>
         <td style="font-size: 14px; line-height: 18px; color: #757575;">
-            <a href="${process.env.HOST_WEB}${newOrder.idUser}/${el.idProduct}">Download</a>
+            <a href="/download/${process.env.HOST_WEB}${newOrder.idUser}/${el.idProduct}">Download</a>
         </td>
         
     </tr>
