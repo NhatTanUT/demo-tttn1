fd6c1f6eb7e4e790fa630a2056155b81dfd571d1
NhatTanUT <tvnt221@gmail.com>
2021-10-12 23:51:41 +0700

    fix category

--- models/category.model.js
+++ models/category.model.js
@@ -11,7 +11,7 @@ const categorySchema = new mongoose.Schema({
         type: mongoose.Schema.Types.ObjectId,
         ref: Product
     }],
-    "category": {
+    "name": {
         type: String
     }
 })
