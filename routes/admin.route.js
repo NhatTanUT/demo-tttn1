6cc6a648c2e2b5baf801814f3f06dd2ff1d4983e
NhatTanUT <tvnt221@gmail.com>
2021-10-15 22:56:46 +0700

    clean code & config auth

--- routes/admin.route.js
+++ routes/admin.route.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const route = express.Router()
+const router = express.Router()
 
 const auth = require('../middleware/auth')
 const {auth_role} = require('../middleware/decentralization')
@@ -8,36 +8,49 @@ const upload_rar = require('../middleware/upload_rar')
 
 const AdminController = require('../controller/admin.controller')
 
-route.get('/', auth, auth_role(['admin']), (req, res) => {
+router.get('/', auth, auth_role(['admin']), (req, res) => {
     res.render('admin')
 })
 
 const uploadMultiple = upload.fields([{ name: 'img', maxCount: 1 }, { name: 'previewImage', maxCount: 6 }])
 
 
-route.post('/register', AdminController.register, AdminController.sendMailRegister)
-route.post('/product', uploadMultiple, AdminController.addProduct )
-route.post('/category', AdminController.addCategory)
-route.patch('/product', uploadMultiple, AdminController.updateProduct)
-route.patch('/category', AdminController.updateCategory)
-route.delete('/product', AdminController.removeProduct)
-route.delete('/category', AdminController.removeCategory)
-route.get('/user', AdminController.getAllUser)
-route.post('/user', AdminController.addUser)
-route.patch('/user', AdminController.updateUser)
-route.delete('/user', AdminController.deleteUser)
-route.get('/order', AdminController.getAllOrder)
-route.delete('/order', AdminController.removeOrder)
-route.post('/sendMailWishlist', AdminController.sendMailWishList)
-route.post('/sendPromotion', AdminController.sendPromotion)
-route.get('/clientOnline', AdminController.getAllClientOnline)
-route.post('/notify', AdminController.sendNotification)
-route.post('/notify_banner', AdminController.sendNotificationBanner)
-// route.post('/removeNotify', AdminController.removeNotify)
-route.post('/uploadrar', upload_rar.single('file'), AdminController.uploadRar)
-route.post('/discount', AdminController.addDiscount)
-route.get('/discount', AdminController.getDiscount)
-route.patch('/discount', AdminController.updateDiscount)
-route.delete('/discount', AdminController.removeDiscount)
+router.post('/register', AdminController.register, AdminController.sendMailRegister)
 
-module.exports = route
+router.route('/product')
+    .post(uploadMultiple, AdminController.addProduct )
+    .patch(uploadMultiple, AdminController.updateProduct)
+    .delete(AdminController.removeProduct)
+
+router.route('/category')
+    .post(AdminController.addCategory)
+    .patch(AdminController.updateCategory)
+    .delete(AdminController.removeCategory)
+
+router.route('/user')
+    .get(AdminController.getAllUser)
+    .post(AdminController.addUser)
+    .patch(AdminController.updateUser)
+    .delete(AdminController.deleteUser)
+
+router.route('/order')
+    .get(AdminController.getAllOrder)
+    .delete(AdminController.removeOrder)
+    
+router.route('/discount')
+    .get(AdminController.getDiscount)
+    .post(AdminController.addDiscount)
+    .patch(AdminController.updateDiscount)
+    .delete(AdminController.removeDiscount)
+
+router.post('/sendMailWishlist', AdminController.sendMailWishList)
+router.post('/sendPromotion', AdminController.sendPromotion)
+router.get('/clientOnline', AdminController.getAllClientOnline)
+router.post('/notify', AdminController.sendNotification)
+router.post('/notify_banner', AdminController.sendNotificationBanner)
+// route.post('/removeNotify', AdminController.removeNotify)
+
+router.post('/uploadrar', upload_rar.single('file'), AdminController.uploadRar)
+
+
+module.exports = router
