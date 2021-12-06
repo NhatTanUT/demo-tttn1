90c7e3ae4fab9825a6bc695b25ef2c58d2646548
NhatTanUT <tvnt221@gmail.com>
2021-12-06 22:58:50 +0700

    a

--- controller/admin.controller.js
+++ controller/admin.controller.js
@@ -171,7 +171,7 @@ class AdminController {
       const subject = "Your wishlist product has changed in price";
 
       const foundProduct = await Product.findOne({
-        _id: mongoose.Types.ObjectId(idProduct),
+        id: mongoose.Types.ObjectId(idProduct),
       });
 
       const body = `<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
